575b98b7ce14f86fff3d08e11b750fc24af8c219
배윤석-닷 <yoonseok.bae98@dotincorp.com>
2023-04-27 15:22:02 +0900

    메인 헤더 관련 수정

--- src/components/navigator/MainHeader.vue
+++ src/components/navigator/MainHeader.vue
@@ -1,26 +1,26 @@
 <template>
-
-    <div id="header-wrap">
-
-        <p id="h-title">이것은 타이틀 </p>
-
-        <ul id="nav-menu">
-
-            <router-link v-for="list in lists" :key="list.key" :to="list.link" slots="li" class="header-menu-list" class-active="active" exact>{{ list.title }} </router-link>
-
-        </ul>
-
-    </div>
-
+    <v-navigation-drawer id="header-wrap" expand-on-hover rail>
+        <v-list density="compact" nav v-for="route in $router.options.routes" :key="route.path">
+            <router-link :to="route.path" class="hyper-link" active-class="active-link">
+                <!-- <v-list-item prepend-icon="mdi-home-circle"> -->
+                <v-list-item prepend-icon="mdi-home-circle">
+                    {{ route.name }}
+                    <!-- <v-list-item-icon v-bind:src="route.asset"></v-list-item-icon> -->
+                    <!-- <v-list-item-content>{{ route.name }}</v-list-item-content> -->
+                </v-list-item>
+            </router-link>
+        </v-list>
+    </v-navigation-drawer>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            lists: [{ link: "/", title: "Home" }, { link: "/about", title: "About" }, { link: "/todoList", title: "Todo" }],
-        };
-    },
-};
-</script>
+<script></script>
 
+<style scoped>
+.hyper-link {
+    text-decoration: none;
+}
+.active-link {
+    background-color: orange !important;
+    color: red;
+}
+</style>
