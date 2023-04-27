8b116a27a37d10f14c7b094b726ada2daf5c69e8
배윤석-닷 <yoonseok.bae98@dotincorp.com>
2023-04-27 12:55:49 +0900

    Vuetify 환경구성

---
+++ src/components/navigator/MainHeader.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div id="header-wrap">
+
+        <p id="h-title">이것은 타이틀 </p>
+
+        <ul id="nav-menu">
+
+            <router-link v-for="list in lists" :key="list.key" :to="list.link" slots="li" class="header-menu-list" class-active="active" exact>{{ list.title }} </router-link>
+
+        </ul>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            lists: [{ link: "/", title: "Home" }, { link: "/about", title: "About" }, { link: "/todoList", title: "Todo" }],
+        };
+    },
+};
+</script>
+
---
+++ src/views/TodoListView.vue
@@ -0,0 +1,5 @@
+<template>
+
+    <div>이것은 todolist 페이지</div>
+
+</template>
